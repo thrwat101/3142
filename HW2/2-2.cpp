3718e7c310730139cac21d62abb917e54dec7a36
Thrwat Najjar <thrwat101@gmail.com>
2021-03-21 13:54:07 -0400

    adding 2.2

---
+++ HW2/2-2.cpp
@@ -0,0 +1,32 @@
+// Cisc 3142
+// Homework 2.2
+// Thrwat Najjar 
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+enum class Gender{ male = 1 , female = 2 };
+
+class TTPlayer {
+    char *name;
+    Gender gender;
+    
+    public:
+        TTPlayer(const char* s, Gender g);
+
+    
+};
+
+int main(){
+    TTPlayer players[] = {
+        {"Player1", Gender::male},
+        {"Player2", Gender::female},
+        {"Player3", Gender::male},
+        {"Player4", Gender::female},
+        {"Player5", Gender::male}
+    };
+
+    return 0;
+}
---
+++ HW2/ordPair.hpp
@@ -0,0 +1,52 @@
+// Cisc 3142
+// Homework 2.2
+// Thrwat Najjar
+
+template <class T> class ordPair{
+   private:
+      T first;
+      T second;
+
+   public:
+      OrdPair(T&, T&);
+      T getFirst();
+      T getSecond();
+      void setFirst (T);
+      void setSecond (T);
+      std::string toString ();
+      bool operator==(const OrdPair &other);
+   
+   ordPair<T>::ordPair(T &fir, T &sec){
+      if(!(fir < sec))
+         throw std::invalid_argument ("Invalid order. First must be less than Second.");
+      
+      else { 
+         first { std::move(fir) };
+         second { std::move(sec) };
+      }
+      
+   }
+
+   T ordPair<T>::getFirst(){ return first; }  
+
+   T ordPair<T>::getSecond(){ return second; }
+
+   void ordPair<T>::setFirst(T fir){
+      if (fir < second) first = fir;
+      else throw std::invalid_argument ("Invalid number.");
+   }
+
+   void ordPair<T>::setSecond(T sec){
+    if (first < sec) second = sec;
+    else throw std::invalid_argument ("Invalid number.");
+   }
+   
+   std::string ordPair<T>::toString() {
+      return first.toString () + " | " + second.toString ();
+   }
+
+   bool ordPair<T>::operator==(const OrdPair &other) {
+      return ((first == other.getFirst()) && (second == other.getSecond()));
+   }
+
+};
