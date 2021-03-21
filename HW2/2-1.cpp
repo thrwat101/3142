64f274df80a49d651b76422ba49f9442de61cf27
Thrwat Najjar <thrwat101@gmail.com>
2021-03-21 10:15:24 -0400

    Update 2-1.cpp

--- HW2/2-1.cpp
+++ HW2/2-1.cpp
@@ -49,16 +49,6 @@ int strLen(const char* str) {
 
 // Compares two strings
 int strCmp(const char* s1, const char* s2) {
-    int i {0};
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] != s2[i]) {
-            if (s1[i] > s2[i]) 
-                return -1;
-            else return 1;
-        }
-        i++;
-    }
-    
     // if s1 is shorter
     if (strLen(s1) < strLen(s2))
         return 1;
