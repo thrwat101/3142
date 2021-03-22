1a31f25e1f6b3c521475ceb353ea7328d432f4fa
Manahil Sher <manahil.sher@gmail.com>
2021-03-22 11:33:27 +0500

    tweaked ordPair and TTPlayer

--- HW2/2-2.cpp
+++ HW2/2-2.cpp
@@ -2,26 +2,53 @@
 // Homework 2.2
 // Thrwat Najjar 
 
-#include <iostream>
+//#include <iostreaclTabCtrlm>
 #include <cstring>
+#include <iostream>
+
+#include "ordPair.hpp"
 
 using namespace std;
 
 enum class Gender{ male = 1 , female = 2 };
 
 class TTPlayer {
-    char *name;
-    Gender gender;
-    
     public:
+        // change these back to private
+        const char *name;
+        Gender gender;
+        
+        // not inline, see below
         TTPlayer(const char* s, Gender g);
-        TTPlayer(const TTPlayer& ttp);
-        std::string toString();
-        bool operator<(const TTPlayer& rhs);
-
-    
-    
+        
+        // copy constructor
+        TTPlayer(const TTPlayer& ttp) :name{ttp.name}, gender{ttp.gender} {}
+        
+        std::string toString() {
+            std::string sname = "";
+            for (int i = 0; i < sizeof(name); i++) { 
+                sname = sname + name[i]; 
+            }
+            
+            std::string sgender = gender == Gender::male ? "M" : "F";
+            
+            return sname + ", " + sgender;
+        }
+        
+        bool operator<(const TTPlayer& rhs) const {
+            cout << "hello" << endl;
+            return true;
+//            return rhs.gender < gender;
+        }
+        
+        
 };
+
+// not inline constructor
+TTPlayer::TTPlayer(const char* s, Gender g) {
+    name = &s[0];
+    gender = {g};
+}
 
 int main(){
     TTPlayer players[] = {
@@ -31,6 +58,9 @@ int main(){
         {"Player4", Gender::female},
         {"Player5", Gender::male}
     };
+    
+    cout << players[1].name << endl;
+//    cout << players[1].<(players[2]) << endl;
 
     return 0;
 }
