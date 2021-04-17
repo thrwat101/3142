cf6295bf3c91ed2ae2db6cf7102ad12dbcb7c9b2
Thrwat Najjar <thrwat101@gmail.com>
2021-04-17 17:36:29 -0400

    file check

--- HW3/3-2.cpp
+++ HW3/3-2.cpp
@@ -25,55 +25,58 @@ int main(){
     ifstream phoneno;
     phoneno.open("phoneno.txt");
 
-    if(!phoneno.good()){
+    if(phoneno.fail()){
         cout << "Trouble opening file.";
         return 1;
-    }
+        
+    } else{
      
-    string line;
-    int numbers = 0;
-    // Reading file into our map
-    while(phoneno) { 
-        getline(phoneno, line); 
+        string line;
+        int numbers = 0;
+        // Reading file into our map
+        while(phoneno) { 
+            getline(phoneno, line); 
 
-        stringstream geek(line); 
-        int id = 0; 
-        geek >> id;
-        string phone_num = line.substr(6, 12);
-        
-        // Storing the data into the map
-        mp[id] = phone_num;
-        
-        string area_code = line.substr(6, 3);
-        
-        // Counting all the numbers that has areaCode 347  
-        if(area_code == "347")
-            numbers++;
-        
-        /* finding all numbers with area code 212. 
-        with and without duplicates */
-        if(area_code == "212"){
-            ac212.push_back(phone_num);
-            set212.insert(phone_num);
-        }
+            stringstream geek(line); 
+            int id = 0; 
+            geek >> id;
+            string phone_num = line.substr(7, 12);
+            
+            // Storing the data into the map
+            mp[id] = phone_num;
+            
+            string area_code = line.substr(7, 3);
+            
+            // finding all numbers with area code 347  
+            if(area_code == "347")
+                numbers++;
+            
+            /* finding all numbers with area code 212 
+            with and without duplicates */
+            if(area_code == "212"){
+                ac212.push_back(phone_num);
+                set212.insert(phone_num);
+            }
 
-    } 
-    
-    // Finding the id's associated with number 212-536-6331
-    vector<int> ids212;
-    for(auto i : mp)
-        if(i.second == "456-789-1234")
-            ids212.push_back(i.first);
-    
-    cout << "Size of the map: " << mp.size() << endl;
-    cout << "Count of phone# with area code 347: " << numbers << endl;
-    cout << "The ID(s) with the given phone#: ";
-    for(int i = 0; i < ids212.size(); i++)
-        cout << ids212[i] << " ";
-    cout << endl;
-    cout << "Size of the vector with all (212) numbers: " << ac212.size() << endl;
-    cout << "Size of the vector with unique (212) numbers: " << set212.size() << endl;
-    
-    // close file 
-    phoneno.close(); 
+        } 
+        
+        // Finding the id's associated with number 212-536-6331
+        vector<int> ids212;
+        for(auto i : mp)
+            if(i.second == "456-789-1234")
+                ids212.push_back(i.first);
+        
+        cout << "Size of the map: " << mp.size() << endl;
+        cout << "Count of phone# with area code 347: " << numbers << endl;
+        cout << "The ID(s) with the given phone#: ";
+        for(int i = 0; i < ids212.size(); i++)
+            cout << ids212[i] << " ";
+        cout << endl;
+        cout << "Size of the vector with all (212) numbers: " << ac212.size() << endl;
+        cout << "Size of the vector with unique (212) numbers: " << set212.size() << endl;
+        
+        // close file 
+        phoneno.close(); 
+    }
+
 }
